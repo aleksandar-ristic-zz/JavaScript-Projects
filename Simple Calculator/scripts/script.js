ff9655b47ffd2357833c3774370d63d323ac5774
Aleksandar <aristic665@gmail.com>
2020-04-20 20:36:47 +0200

    Second commit
    Added comments

--- Simple Calculator/scripts/script.js
+++ Simple Calculator/scripts/script.js
@@ -3,13 +3,15 @@
 * 
 */
 
+// Main Function
 function calculate() {
-
+    
+    //Taking numbers and operator from input
     var a = parseFloat(document.querySelector("#integerA").value);
     var b = parseFloat(document.querySelector("#integerB").value);
     var op = document.querySelector("#operators").value;
 
-
+    // If-else chain for basic arithmetic operations
     if(op === '+') {
         let c = a + b;
         document.getElementById("result").innerHTML = a + " + " + b + " = " + c;
@@ -22,6 +24,7 @@ function calculate() {
         let c = a * b;
         document.getElementById("result").innerHTML = a + " * " + b + " = " + c;
 
+    // Making if-else chain inside divison operator, for dividing by zero
     } else if(op ==='/') {
         if(a !== 0) {
             let c = a / b;
@@ -32,7 +35,7 @@ function calculate() {
             document.getElementById("result").innerHTML = "Result is Infinity!";
         }
         
-
+    //If some other character is entered inside operator input field
     } else {
         document.getElementById("result").innerHTML = "Use only: +, -, * and /";
     }
